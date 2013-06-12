0d2df3458779d99b7a97c688da7dfb6edc3c88f2
Jørgen Jacobsen <jorgen.jacobsen@fronter.com>
2013-06-12 16:14:21 +0200

    Added support for image gallery-row with clickable images that expand

--- js/hacklife/hacklife.js
+++ js/hacklife/hacklife.js
@@ -11,6 +11,8 @@ var hacklife = {
     contentWrapper : $('.content-wrapper'),
     articleWrapper : $('.article-wrapper'),
     articleHeader : $('.article-header'),
+    galleryImage : $('.gallery-row img'),
+    galleryFullscrenImage : $('.fullscreen img'),
     articleP : $('article p'),
     openSlideMenu : $('.open-slide-menu'),
     child : $('.child')
@@ -27,14 +29,27 @@ var hacklife = {
   },
   findVideo : function() {
     var video = hacklife.el.articleP.find(".fluid-width-video-wrapper");
-    var photo_temp = hacklife.el.articleHeader[0].outerHTML;
-    if(video.length) {
-      hacklife.el.articleHeader[0].outerHTML = video[0].outerHTML;
-      video[0].outerHTML = photo_temp;
+    if(hacklife.el.articleHeader.length) {
+      var photo_temp = hacklife.el.articleHeader[0].outerHTML;
+      if(video.length) {
+        hacklife.el.articleHeader[0].outerHTML = video[0].outerHTML;
+        video[0].outerHTML = photo_temp;
+      }
     }
   },
   bindUIElements : function () {
     hacklife.el.browserDocument.on("scroll", hacklife.toggleHeader);
+    hacklife.el.galleryImage.on("click", hacklife.openImage);
+  },
+  openImage : function (event) {
+    if($(this).parent().hasClass('fullscreen')) {
+      $(this).unwrap();
+    } else {
+      $(this).wrap('<div class="fullscreen" />');
+    }
+  },
+  closeImage : function (event) {
+    $(this).child()
   },
   toggleHeader : function () {
     if(hacklife.el.browserDocument.scrollTop() > 480 && !hacklife.gvar.headerToggle) {

99cda681557fd95b131ceb39b621b0da56e80c2d
Jørgen Jacobsen <jorgeja@gmail.com>
2013-07-24 22:56:46 +0200

    Removed config.js from src/scripts folder

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,12 +1,12 @@
 /** Rename this to config.js and change baseUrl path **/
 
 requirejs.config({
-    baseUrl : "../scripts/lib",
+    baseUrl : '/wp-content/themes/html5blank-master/hack/src/scripts/lib',
     paths : {
-      hacklife : "../hacklife",
-      jquery  : "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-      fitvids  : "jquery.fitvids.require"
+      hacklife : '../hacklife',
+      jquery  : '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+      fitvids  : 'jquery.fitvids.require'
     }
 });
 
-requirejs(["hacklife/main"]);
+requirejs(['hacklife/main']);

ee75fc15eea11712f0c02f4477b68cd51e68d5d2
Jørgen Jacobsen <jorgen.jacobsen@fronter.com>
2013-07-25 16:31:44 +0200

    small changes to style

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,7 +1,7 @@
 /** Rename this to config.js and change baseUrl path **/
 
 requirejs.config({
-    baseUrl : '/wp-content/themes/html5blank-master/hack/src/scripts/lib',
+    baseUrl : '../scripts/lib',
     paths : {
       hacklife : '../hacklife',
       jquery  : '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',

b53b2f5af4794487506e59023c479f95bbaf04d0
Jørgen Jacobsen <jorgeja@gmail.com>
2013-11-06 23:57:22 +0100

    Changed style slightly

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -4,8 +4,9 @@ requirejs.config({
     baseUrl : '/hacklivetditt/wp-content/themes/hacklivet/hack/src/scripts/lib',
     paths : {
       hacklife : '../hacklife',
-      jquery  : '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
-      fitvids  : 'jquery.fitvids.require'
+      jquery  : 'jquery-2.0.3',
+      fitvids  : 'jquery.fitvids.require',
+      
     }
 });
 
